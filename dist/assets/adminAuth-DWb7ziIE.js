5e9a019ca8361419546633e61a99ac32bdc6be4b
davemelk100 <davemelk@gmail.com>
2025-07-21 12:43:03 -0400

    build fix

---
+++ dist/assets/adminAuth-DWb7ziIE.js
@@ -0,0 +1,1 @@
+import{d as i}from"./index-y4cvH10Z.js";const e=()=>!0,s=()=>{try{i.clearSession()}catch(e){}},t=()=>({isActive:!0,expiresAt:null,timeRemaining:"No expiration"});export{e as c,t as g,s as l};
--- dist/assets/adminAuth-Pj83uCBZ.js
+++
@@ -1,1 +0,0 @@
-import{d as i}from"./index-BE1V87Gk.js";const e=()=>!0,s=()=>{try{i.clearSession()}catch(e){}},t=()=>({isActive:!0,expiresAt:null,timeRemaining:"No expiration"});export{e as c,t as g,s as l};
