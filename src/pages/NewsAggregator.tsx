64b5090a66d1df5742ce05ea2d05f74aad224ac1
davemelk100 <davemelk@gmail.com>
2025-08-29 22:43:31 -0400

    lots

--- src/pages/NewsAggregator.tsx
+++ src/pages/NewsAggregator.tsx
@@ -3557,7 +3557,7 @@ const NewsAggregator = () => {
                 }`}
               >
                 <svg
-                  className="w-7 h-7"
+                  className="w-8 h-8"
                   fill="none"
                   stroke="currentColor"
                   viewBox="0 0 24 24"
